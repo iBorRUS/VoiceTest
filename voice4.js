3b8ee76bd289184bfd7e2148f7abe534aea2353a
iBorRUS <46121417+iBorRUS@users.noreply.github.com>
2018-12-25 23:19:58 +0500

    Add files via upload

--- voice4.js
+++ voice4.js
@@ -40,7 +40,7 @@ recognizer.onstart = function(){
 
 recognizer.onend = function(){                    	// Закончилось время ожидания (примерно 15 сек)
   console.log('recognizer.onend');
-  strvoice("Отключаюсь.");
+  strvoice("Жду команду");
   //document.getElementById('micbutton').classList.remove("miganie");	// убрать МИГАНИЕ МИКРОФОНА
   //strcommand="";
   recognizer.start();
